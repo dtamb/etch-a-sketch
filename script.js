15fc7ff6e552eb98d0352a32cbd44925ed037775
dtamb <89025060+dtamb@users.noreply.github.com>
2024-06-29 16:12:23 +1000

    Limit canvas to 960px and variable grid
    Canvas is now 960px regardless of number of grids.
    The grid function now takes input n to create an
    n * n grid.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const container = document.querySelector(".container");
 
-function createGrid(){
-    for (i=0;i<16;i++){
+function createGrid(gridLength){
+    for (i=0;i<gridLength;i++){
         const row = document.createElement("div");
         row.classList.add("row");
         container.appendChild(row);
-        for (j=0;j<16;j++){
+        for (j=0;j<gridLength;j++){
             const pixel = document.createElement("div")
             pixel.classList.add("pixel");
             row.appendChild(pixel);
@@ -13,18 +13,19 @@ function createGrid(){
     };
 }
 
-createGrid();
+createGrid(80);
 
-container.addEventListener("mouseover",(event)=>{
+container.addEventListener("mousemove",(event)=>{
     let target = event.target;
     if (target.classList.value === "pixel"){
         target.classList.toggle("colour");
     };
 });
 
+/*
 container.addEventListener("mouseout",(event)=>{
     let target = event.target;
     if (target.classList.value === "pixel"){
         target.classList.toggle("colour");
     }
-});
+});*/
