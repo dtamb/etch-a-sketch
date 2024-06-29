da6f0f3147b9d786fe2b7f80135625a7a30a0afe
dtamb <89025060+dtamb@users.noreply.github.com>
2024-06-29 16:36:52 +1000

    Random RGB on hover. Complete

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector(".container");
+const sizeButton = document.querySelector(".sizeButton");
 
 function createGrid(gridLength){
     for (i=0;i<gridLength;i++){
@@ -9,23 +10,32 @@ function createGrid(gridLength){
             const pixel = document.createElement("div")
             pixel.classList.add("pixel");
             row.appendChild(pixel);
-        }
+        };
     };
 }
 
-createGrid(80);
+function randomRGB(){
+    let red = Math.floor(Math.random()*256);
+    let green = Math.floor(Math.random()*256);
+    let blue = Math.floor(Math.random()*256);
+    return (`rgb(${red},${green},${blue})`);
+}
+
+// Initialising grid
+createGrid(16);
 
 container.addEventListener("mousemove",(event)=>{
     let target = event.target;
     if (target.classList.value === "pixel"){
-        target.classList.toggle("colour");
+        RGB = randomRGB();
+        target.style.cssText = `background: ${RGB};`;
     };
 });
 
-/*
-container.addEventListener("mouseout",(event)=>{
-    let target = event.target;
-    if (target.classList.value === "pixel"){
-        target.classList.toggle("colour");
-    }
-});*/
+sizeButton.addEventListener("click",()=>{
+    container.replaceChildren();
+    let gridSize = prompt('Enter a number from 1 - 100');
+    if (gridSize > 100) gridSize = 100;
+    else if (gridSize < 1) gridSize = 1;
+    createGrid(gridSize);
+});
